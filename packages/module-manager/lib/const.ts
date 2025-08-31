f108d8176e9c220543aa9750695792449cd3cc42
jerryhuangyu <jerry0932619628@gmail.com>
2025-08-31 12:51:29 +0800

    chore: update E2E test scenarios and remove unused options from choices

--- packages/module-manager/lib/const.ts
+++ packages/module-manager/lib/const.ts
@@ -6,12 +6,12 @@ export const DEFAULT_CHOICES = [
   { name: "Content Script (Execute JS on Web Page)", value: "content" },
   { name: "Content Script UI (Render Custom React Component on Web Page)", value: "content-ui" },
   { name: "Content Script Runtime (Inject JS on Specific Actions like Popup Click)", value: "content-runtime" },
-  { name: "Background Script", value: "background" },
-  { name: "New Tab Override", value: "new-tab" },
+  // { name: "Background Script", value: "background" },
+  // { name: "New Tab Override", value: "new-tab" },
   { name: "Popup (On Extension Icon Click)", value: "popup" },
-  { name: "DevTools (Include DevTools Panel)", value: "devtools" },
-  { name: "Side Panel", value: "side-panel" },
-  { name: "Options Page", value: "options" },
+  // { name: "DevTools (Include DevTools Panel)", value: "devtools" },
+  // { name: "Side Panel", value: "side-panel" },
+  // { name: "Options Page", value: "options" },
   { name: "All tests", value: "tests" },
 ] as const;
 
